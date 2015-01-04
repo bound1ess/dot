e96cea501a2c2ae472e7441e11713f028054d699
bound1ess <bound1ess.dev@gmail.com>
2015-01-04 22:54:12 +0600

    Essentials. Initial commit.

---
+++ spec/Bound1ess/DotSpec.php
@@ -0,0 +1,12 @@
+<?php namespace spec\Bound1ess;
+
+use PhpSpec\ObjectBehavior;
+
+class DotSpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Bound1ess\Dot');
+    }
+
+}
---
+++ src/Bound1ess/Dot.php
@@ -0,0 +1,3 @@
+<?php namespace Bound1ess;
+
+class Dot {}
